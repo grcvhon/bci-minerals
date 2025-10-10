65ea0be5ecc6e72b7d8cde7dc3bb75fb2dd0c698
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-10-10 13:32:05 +1030

    Update: side plot

--- sampling_design/plot_impact.R
+++ sampling_design/plot_impact.R
@@ -1,39 +1,20 @@
+### Plot impact area
+# This script takes the available information from
+# https://www.epa.wa.gov.au/sites/default/files/Referral_Documentation/Att3_DSDMP%20%28O2%20Marine%2C%202025%29.pdf
+# regarding the dredge and spoil areas of the BCI Minerals Mardie Project
+
+# set working directory
 setwd("C:/Users/a1235304/Dropbox/BCI-minerals/sampling_design")
 
+# load packages
 library(sf)
 library(ggplot2)
 library(mapview)
 library(leaflet)
 
-# Generate polygon for spoil and dredge areas
+# Generate polygon for dredge and spoil areas
 
-### *** SPOIL *** ###
-
-# 1. Read and convert to sf points
-spoil <- read.csv("./impact/spoil.csv")
-spoil_points <- sf::st_as_sf(spoil, coords = c("x", "y"), crs = 4326)
-
-# 2. Extract coordinates and ensure polygon is closed (first point == last point)
-spoil_coords <- sf::st_coordinates(spoil_points)
-
-# If not already closed, add the first point to the end
-if (!all(spoil_coords[1, ] == spoil_coords[nrow(spoil_coords), ])) {
-  spoil_coords <- rbind(spoil_coords, spoil_coords[1, ])
-}
-
-# 3. Create polygon geometry
-spoil_poly_geom <- sf::st_polygon(list(spoil_coords))
-
-# 4. Wrap in an sf object with same CRS
-spoil_area <- sf::st_sfc(spoil_poly_geom, crs = sf::st_crs(spoil_points))
-
-# 5. Convert to sf data frame
-spoil_area <- sf::st_sf(geometry = spoil_area)
-plot(spoil_area)
-
-
-
-### *** DREDGE *** ###
+#### *** DREDGE *** ####
 
 # 1. Read and convert to sf points
 dredge <- read.csv("./impact/dredge.csv")
@@ -59,7 +40,45 @@ plot(dredge_area)
 
 mapview::mapview(spoil_area) + mapview::mapview(dredge_area)
 
-### *** APPROXIMATE EXTENT *** ###
+#### *** SPOIL *** ####
+
+# 1. Read and convert to sf points
+spoil <- read.csv("./impact/spoil.csv")
+spoil_points <- sf::st_as_sf(spoil, coords = c("x", "y"), crs = 4326)
+
+# 2. Extract coordinates and ensure polygon is closed (first point == last point)
+spoil_coords <- sf::st_coordinates(spoil_points)
+
+# If not already closed, add the first point to the end
+if (!all(spoil_coords[1, ] == spoil_coords[nrow(spoil_coords), ])) {
+  spoil_coords <- rbind(spoil_coords, spoil_coords[1, ])
+}
+
+# 3. Create polygon geometry
+spoil_poly_geom <- sf::st_polygon(list(spoil_coords))
+
+# 4. Wrap in an sf object with same CRS
+spoil_area <- sf::st_sfc(spoil_poly_geom, crs = sf::st_crs(spoil_points))
+
+# 5. Convert to sf data frame
+spoil_area <- sf::st_sf(geometry = spoil_area)
+plot(spoil_area)
+
+multi_leaf <- leaflet() %>%
+  addProviderTiles("CyclOSM") %>%
+  addPolygons(data = dredge_area, color = "red", group = "Dredge Area", 
+              weight = 2, fillOpacity = 50, fillColor = "pink") %>%
+  addPolygons(data = spoil_area, color = "red", group = "Spoil Area", 
+              weight = 2, fillOpacity = 50, fillColor  = "pink") %>%
+  addScaleBar(position = "bottomleft", options = scaleBarOptions(imperial = FALSE))
+
+multi_leaf
+
+st_area(dredge_area) # 0.59 km^2
+st_area(spoil_area) # 0.30 km^2
+
+
+#### *** APPROXIMATE EXTENT *** ####
 
 # Approximate extent
 xmin  <- 115.61768 
@@ -75,44 +94,19 @@ nw_shape_crop <- sf::st_crop(nw_shape,
                              ymax = ymax)
 nw_shape_crop <- st_transform(nw_shape_crop, crs = 4326)
 
+nw_shp_crop_leaf <- leaflet() %>% 
+  addProviderTiles("OpenStreetMap") %>% 
+  addPolygons(data = nw_shape_crop, color = "blue", 
+              fillOpacity = 0, weight = 2) %>% 
+  addPolygons(data = dredge_area, color = "red", group = "Dredge Area", 
+              weight = 2, fillOpacity = 50, fillColor = "pink") %>%
+  addPolygons(data = spoil_area, color = "red", group = "Spoil Area", 
+              weight = 2, fillOpacity = 50, fillColor  = "pink") %>%
+  addScaleBar(position = "bottomleft", options = scaleBarOptions(imperial = FALSE))
+nw_shp_crop_leaf
 
 
 
-multi_leaf <- leaflet() %>%
-  addProviderTiles("Esri.WorldImagery") %>%
-  addPolygons(data = nw_shape_crop, color = "darkblue", group = "NW Crop", fill = NA) %>%
-  addPolygons(data = spoil_area, color = "yellow", group = "Spoil Area") %>%
-  addPolygons(data = dredge_area, color = "orangered", group = "Dredge Area") %>%
-  addScaleBar(position = "bottomleft", options = scaleBarOptions(imperial = FALSE)) %>%
-  addLayersControl(
-    overlayGroups = c("NW Crop", "Spoil Area", "Dredge Area"),
-    options = layersControlOptions(collapsed = FALSE)
-  )
-multi_leaf
-mapview::mapshot2(multi_leaf, file = "multi_leaf.png")
 
-ggplot() + 
-  geom_sf(data = spoil_area) +
-  geom_sf(data = nw_shape_crop, fill = NA) +
-  coord_sf(xlim = c(xmin,xmax), ylim = c(ymin,ymax), expand = FALSE)
-  
-
-library(tmap)
-
-tmap_mode("plot")  # for static maps
-
-# Reproject to Web Mercator (EPSG:3857)
-nw_shape_crop_proj <- sf::st_transform(nw_shape_crop, 4326)
-spoil_area_proj <- sf::st_transform(spoil_area, 4326)
-dredge_area_proj <- sf::st_transform(dredge_area, 4326)
-
-tm_shape(nw_shape_crop_proj) +
-  tm_borders(col = "blue") +
-  tm_shape(spoil_area_proj) +
-  tm_borders() +
-  tm_shape(dredge_area_proj) +
-  tm_borders(col = "orangered") +
-  tm_scale_bar(position = c("right", "bottom")) +
-  tm_layout(legend.outside = TRUE)
 
 
